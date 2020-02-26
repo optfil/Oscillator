d149a8a9b7daec3f842210232912d06a57e1a741
A. Dergachev <dergachev88@gmail.com>
2020-02-26 10:15:48 +0300

    DynSystem object created

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "form.h"
+#include "dynsystem.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
-{
+{/*
     QApplication a(argc, argv);
     Form w;
     w.show();
-    return a.exec();
+    return a.exec();*/
+
+    DynSystem sys(1.0, 0.0, 10.0, 1.0, 0.0, 0.0, 1.0e-3);
 }
