f039827cdaf578374c2268d55ee386381f20b474
A. Dergachev <dergachev88@gmail.com>
2020-02-26 10:26:01 +0300

    getters tested

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "form.h"
 #include "dynsystem.h"
 
+#include <QDebug>
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -11,4 +13,7 @@ int main(int argc, char *argv[])
     return a.exec();*/
 
     DynSystem sys(1.0, 0.0, 10.0, 1.0, 0.0, 0.0, 1.0e-3);
+
+    qDebug() << sys.m() << sys.gamma() << sys.k();
+    qDebug() << sys.t() << sys.x() << sys.v();
 }
