572d3cb2f2507bcf936ef49592ebf8ba7b41b191
A. Dergachev <dergachev88@gmail.com>
2020-02-26 10:11:37 +0300

    DydnSystem data defined

--- dynsystem.cpp
+++ dynsystem.cpp
@@ -1,6 +1,5 @@
 #include "dynsystem.h"
 
-DynSystem::DynSystem()
-{
-
-}
+DynSystem::DynSystem(double m, double gamma, double k, double x0, double v0, double t, double dt):
+    m_(m), gamma_(gamma), k_(k), x_(x0), v_(v0), t_(t), dt_(dt)
+{}
--- dynsystem.h
+++ dynsystem.h
@@ -5,7 +5,16 @@
 class DynSystem
 {
 public:
-    DynSystem();
+    DynSystem(double m, double gamma, double k, double x0, double v0, double t, double dt);
+
+private:
+    double m_;
+    double gamma_;
+    double k_;
+    double x_;
+    double v_;
+    double t_;
+    double dt_;
 };
 
 #endif // DYNSYSTEM_H
