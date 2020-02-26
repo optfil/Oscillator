8d27558b137a45b35ba652fb6a886e1ceeef5999
A. Dergachev <dergachev88@gmail.com>
2020-02-26 10:20:25 +0300

    getters implemented

--- dynsystem.h
+++ dynsystem.h
@@ -7,6 +7,14 @@ class DynSystem
 public:
     DynSystem(double m, double gamma, double k, double x0, double v0, double t, double dt);
 
+    double m() const {return m_;}
+    double gamma() const {return gamma_;}
+    double k() const {return k_;}
+    double x() const {return x_;}
+    double v() const {return v_;}
+    double t() const {return t_;}
+    double dt() const {return dt_;}
+
 private:
     double m_;
     double gamma_;
