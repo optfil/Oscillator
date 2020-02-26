5950726b65b04d742b534488e3da5da0e174ca02
A. Dergachev <dergachev88@gmail.com>
2020-02-26 10:36:16 +0300

    euler method tested

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "form.h"
 #include "dynsystem.h"
 
+#include <cmath>
+
 #include <QDebug>
 
 #include <QApplication>
@@ -12,8 +14,8 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();*/
 
-    DynSystem sys(1.0, 0.0, 10.0, 1.0, 0.0, 0.0, 1.0e-3);
+    DynSystem sys(1.0, 0.0, 10.0, 1.0, 0.0, 0.0, 1.0e-3 * 2.0*M_PI*std::sqrt(1.0/10.0));
     qDebug() << sys.t() << sys.x() << sys.v();
-    sys.step();
+    for (int n = 0; n < 500; ++n) sys.step();
     qDebug() << sys.t() << sys.x() << sys.v();
 }
