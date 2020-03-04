179c7908b110d960e5ce0aa69ab2199eda43b07a
A. Dergachev <dergachev88@gmail.com>
2020-03-04 10:09:26 +0300

    widgets initialized

--- form.cpp
+++ form.cpp
@@ -3,6 +3,24 @@
 Form::Form(QWidget *parent)
     : QWidget(parent)
 {
+    labelM = new QLabel("mass");
+    labelGamma = new QLabel("gamma");
+    labelK = new QLabel("rigidity");
+    labelInitX = new QLabel("initial coordinate");
+    labelInitV = new QLabel("initial velocity");
+    labelDt = new QLabel("time step");
+
+    doubleSpinBoxM = new QDoubleSpinBox;
+    doubleSpinBoxGamma = new QDoubleSpinBox;
+    doubleSpinBoxK = new QDoubleSpinBox;
+    doubleSpinBoxInitX = new QDoubleSpinBox;
+    doubleSpinBoxInitV = new QDoubleSpinBox;
+    doubleSpinBoxDt = new QDoubleSpinBox;
+
+    pushButtonStart = new QPushButton("Start");
+    pushButtonStop = new QPushButton("Stop");
+
+    textEditLog = new QTextEdit;
 }
 
 Form::~Form()
