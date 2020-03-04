a07d23983e9f53bfb4c418b40aa265925ac15c69
A. Dergachev <dergachev88@gmail.com>
2020-03-04 09:30:47 +0300

    main() restored

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@
 #include <QApplication>
 
 int main(int argc, char *argv[])
-{/*
+{
     QApplication a(argc, argv);
     Form w;
     w.show();
-    return a.exec();*/
-
+    return a.exec();
+/*
     DynSystem sys(1.0, 0.0, 10.0, 1.0, 0.0, 0.0, 1.0e-3 * 2.0*M_PI*std::sqrt(1.0/10.0));
     qDebug() << sys.t() << sys.x() << sys.v();
     for (int n = 0; n < 500; ++n) sys.step();
-    qDebug() << sys.t() << sys.x() << sys.v();
+    qDebug() << sys.t() << sys.x() << sys.v();*/
 }
